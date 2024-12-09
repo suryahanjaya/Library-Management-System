310b930f6a307c92a942faf63bb2dea744290e03
deaprima <deaprima209@gmail.com>
2024-12-09 13:42:09 +0700

    fix window action

--- web/js/script.js
+++ web/js/script.js
@@ -1,5 +1,24 @@
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+document.onkeydown = (e) => {
+    if (e.key == 123) {
+        e.preventDefault(); 
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+        e.preventDefault(); 
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+        e.preventDefault(); 
+    }
+    if (e.ctrlKey && e.key == 'U') {
+        e.preventDefault(); 
+    }
+};
+
 function logout() {
-    // Clear user data from localStorage
+
     localStorage.clear();
 
     window.location.href = 'login.html';
